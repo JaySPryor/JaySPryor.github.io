f152a3b9b9a11406f5c6ea7364b26f083c3c16a7
JayP <86741921+JaySPryor@users.noreply.github.com>
2021-09-13 22:39:05 -0600

    moved phone validation to bottom of js file

--- JS/JP_Portfolio.js
+++ JS/JP_Portfolio.js
@@ -1,18 +1,6 @@
 /******** JAVASCRIPT FOR PORTFOLIO WEBSITE *********/
 
-//Form Validation
-function validatePhone() {
-    //gets value of input with id="numb"
-    let phoneNumber = document.getElementById("Phone").value;
-    //If phoneNumber is not a number:
-    let text;
-    if (isNaN(phoneNumber)) {
-        text = "Must be a valid phone number";
-    } else {
-        text = "";
-    }
-    document.getElementById("error").innerHTML = text;
-}
+
 
 //Opens contact form.
 function openForm() {
@@ -52,4 +40,18 @@ function showSlides(n) {
     }
     slides[slideIndex - 1].style.display = "block"; // This displays the image in the slideshow
     dots[slideIndex - 1].className += " active"; // This adds the active styling to the dot associated with the image
+}
+
+//Form Validation
+function validatePhone() {
+    //gets value of input with id="numb"
+    let phoneNumber = document.getElementById("Phone").value;
+    //If phoneNumber is not a number:
+    let text;
+    if (isNaN(phoneNumber)) {
+        text = "Must be a valid phone number";
+    } else {
+        text = "";
+    }
+    document.getElementById("error").innerHTML = text;
 }
